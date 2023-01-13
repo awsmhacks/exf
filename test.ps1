ca62e0d5962ceabc982840a20609b71d90335348
PoesRaven <cwgraven@gmail.com>
2023-01-13 11:46:13 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,4 @@
 dir;
-$aes = new-object System.Security.Cryptography.AesCryptoServiceProvider;
-$aes.KeySize = 128;
-$aes.Mode = 4;
-$aes_crypt = $aes.CreateEncryptor([System.Security.Cryptography.Rfc2898DeriveBytes]::new("RansomAllTheThings!",@([byte]1,2,3,4,5,6,7,8)).GetBytes(16), @([byte]16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1));
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = “All of your important documents have been encrypted. Pay the ransom… or else!!!”
+[System.Windows.MessageBox]::Show($msgBody)
